e4cf67f8b37ed36190ecfbae90123bc690ee4037
hugobepa <hugo_bcn@hotmail.com>
2025-12-27 11:04:10 +0100

    ProductsApp_configInicial

---
+++ app/(products-app)/(home)/index.tsx
@@ -0,0 +1,13 @@
+import { ThemedText } from "@/presentation/theme/components/themed-text";
+import React from "react";
+import { View } from "react-native";
+
+const HomeScreen = () => {
+  return (
+    <View>
+      <ThemedText>HomeScreen</ThemedText>
+    </View>
+  );
+};
+
+export default HomeScreen;
