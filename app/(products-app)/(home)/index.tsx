122d5089e33c81e058749e192b827092656fe865
hugobepa <hugo_bcn@hotmail.com>
2025-12-27 14:19:48 +0100

    ProductsApp_coloresTexto

--- app/(products-app)/(home)/index.tsx
+++ app/(products-app)/(home)/index.tsx
@@ -1,11 +1,14 @@
 import { ThemedText } from "@/presentation/theme/components/themed-text";
+import { useThemeColor } from "@/presentation/theme/hooks/use-theme-color";
 import React from "react";
 import { View } from "react-native";
 
 const HomeScreen = () => {
+  const primary = useThemeColor({}, "primary");
+
   return (
     <View style={{ paddingTop: 100, paddingHorizontal: 20 }}>
-      <ThemedText style={{ fontFamily: "Kanit_700Bold" }}>
+      <ThemedText style={{ fontFamily: "Kanit_700Bold", color: primary }}>
         HomeScreen
       </ThemedText>
       <ThemedText style={{ fontFamily: "Kanit_400Regular" }}>
