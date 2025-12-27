79828ab111c5e5a75d743dfaae3746c5bc2cdd7e
hugobepa <hugo_bcn@hotmail.com>
2025-12-27 22:57:50 +0100

    ProductsApp_ThemedTextInput

--- presentation/theme/components/ThemedTextInput.tsx
+++ presentation/theme/components/ThemedTextInput.tsx
@@ -1,17 +1,59 @@
 import { Ionicons } from "@expo/vector-icons";
-import React from "react";
-import { Text, TextInputProps, View } from "react-native";
+import React, { useRef, useState } from "react";
+import { StyleSheet, TextInput, TextInputProps, View } from "react-native";
+import { useThemeColor } from "../hooks/use-theme-color";
 
 interface Props extends TextInputProps {
   icon?: keyof typeof Ionicons.glyphMap;
 }
 
 const ThemedTextInput = ({ icon, ...rest }: Props) => {
+  const primaryColor = useThemeColor({}, "primary");
+  const textColor = useThemeColor({}, "text");
+  const [isActive, setIsActive] = useState(false);
+  const inputRef = useRef<TextInput>(null);
   return (
-    <View>
-      <Text>ThemedTextInput</Text>
+    <View
+      style={{
+        ...styles.border,
+        borderColor: isActive ? primaryColor : "#ccc",
+      }}
+      onTouchStart={() => inputRef.current?.focus()}
+    >
+      {icon && (
+        <Ionicons
+          name={icon}
+          size={24}
+          color={textColor}
+          style={{ marginRight: 10 }}
+        />
+      )}
+
+      <TextInput
+        ref={inputRef}
+        onFocus={() => setIsActive(true)}
+        onBlur={() => setIsActive(false)}
+        placeholderTextColor="#5c5c5c"
+        style={{
+          color: textColor,
+          marginRight: 10,
+          flex: 1,
+        }}
+        {...rest}
+      />
     </View>
   );
 };
 
 export default ThemedTextInput;
+
+const styles = StyleSheet.create({
+  border: {
+    borderWidth: 1,
+    borderRadius: 5,
+    padding: 5,
+    marginBottom: 10,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+});
