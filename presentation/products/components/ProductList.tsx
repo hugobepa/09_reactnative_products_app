9d8e92ee2ff9d56752e06f758b342cf9ed028a5d
hugobepa <hugo_bcn@hotmail.com>
2025-12-28 21:32:20 +0100

    ProductsApp_ScrollInfinito

--- presentation/products/components/ProductList.tsx
+++ presentation/products/components/ProductList.tsx
@@ -1,6 +1,7 @@
 import { Product } from "@/core/products/interface/product.interface";
-import React from "react";
-import { FlatList } from "react-native";
+import { useQueryClient } from "@tanstack/react-query";
+import React, { useState } from "react";
+import { FlatList, RefreshControl } from "react-native";
 import { ProductCard } from "./ProductCard";
 
 interface Props {
@@ -9,12 +10,28 @@ interface Props {
 }
 
 const ProductList = ({ products, loadNextPage }: Props) => {
+  const [isRefreshing, setIsRefreshing] = useState(false);
+  const queryClient = useQueryClient();
+  const onPullToRefresh = async () => {
+    setIsRefreshing(true);
+    await new Promise((resolve) => setTimeout(resolve, 200));
+    //invalidar cache mediante tanStack
+    queryClient.invalidateQueries({ queryKey: ["products", "infitive"] });
+    setIsRefreshing(false);
+  };
+
   return (
     <FlatList
       data={products}
       numColumns={2}
       keyExtractor={(item) => item.id} //string
       renderItem={({ item }) => <ProductCard product={item} />}
+      onEndReached={loadNextPage}
+      onEndReachedThreshold={0.8}
+      showsVerticalScrollIndicator={false}
+      refreshControl={
+        <RefreshControl refreshing={isRefreshing} onRefresh={onPullToRefresh} />
+      }
     />
   );
 };
